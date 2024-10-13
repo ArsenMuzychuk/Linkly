35cd2a582031e760950935b5b13a91df54f94b33
ArsenMuzychuk <arsicmuz@gmail.com>
2024-10-13 09:10:13 +0300

    Update sgl.py

--- venv/sgl.py
+++ venv/sgl.py
@@ -1,35 +1,36 @@
 import mysql.connector
-
-# Підключення до бази даних
 db = mysql.connector.connect(
     host="localhost",
     user="root",
     password="root",
-    database="Linkly",
     port="8889"
 )
 
 cur = db.cursor()
-
-# Створення таблиці
+cur.execute("CREATE DATABASE IF NOT EXISTS Linkly;")
+cur.execute("USE Linkly;")
 cur.execute("""
-    CREATE TABLE IF NOT EXISTS items (
+    CREATE TABLE IF NOT EXISTS Users (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(100) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL
+        user_name VARCHAR(50),
+        email VARCHAR(100) UNIQUE,
+        password VARCHAR(255),
+        date DATE
     );
 """)
-
-# Додавання записів до таблиці
 cur.execute("""
-    INSERT INTO items (name, email, password)
-    VALUES ('Іван Петренко', 'ivan@example.com', 'password123'),
-           ('Марія Коваль', 'maria@example.com', 'password456'),
-           ('Олександр Сидоренко', 'olex@example.com', 'password789');
+    CREATE TABLE IF NOT EXISTS item (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        last_name VARCHAR(40) DEFAULT 'Undefined',
+        first_name VARCHAR(40) DEFAULT 'Undefined',
+        status ENUM('newcomer', 'ancient', 'experienced') DEFAULT 'newcomer' NOT NULL,
+        location VARCHAR(45) NOT NULL,
+        info TEXT DEFAULT NULL,
+        links TEXT DEFAULT NULL,
+        other_table_id INT DEFAULT NULL,
+        CONSTRAINT fk_other_table_id FOREIGN KEY (other_table_id) REFERENCES Users(id) ON DELETE SET NULL
+    );
 """)
-
-# Збереження змін
 db.commit()
 cur.close()
 db.close()
